a2550c9f724707f29b7d6587ab250e4491875062
Sohaib Talaat Bhatti <sohaibbbhatti@gmail.com>
2016-01-15 17:17:42 +0500

    Pagination of assets

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,20 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   page: 1,
   model(params) {
-    
-    return this.store.query('asset',{page: '1'});
+    return this.store.findAll('asset');
   },
 
  actions: {
     dosomethingplz: function() {
         
       this.store.query('asset', { page: '2' } ).then((json) =>   {
-        var assets = [];
-        assets.pushObjects(json.toArray());
-        assets.pushObjects(this.currentModel.toArray());
-        this.set('currentModel', assets);
-        //debugger;
-        //this.currentModel.addObjects(json.toArray());
+        this.refresh();
         });
       }
 
